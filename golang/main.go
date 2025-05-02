f3e3683906d113903de6ebbcc5ea5c1cbc10328a
Vladimir Müller <code@vladmueller.de>
2025-05-02 11:58:32 +0200

    🐹 Exception handling concept

--- golang/main.go
+++ golang/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
 func main() {
-	println("Hello, World!")
+	a := float64(42)
+	b := float64(0)
+
+	result, err := divide(a, b)
+	if err != nil {
+		// handle error
+		fmt.Println(err)
+	}
+
+	fmt.Println(result)
+}
+
+// NOTE: a function in Go can return multiple return values
+
+func divide(left float64, right float64) (float64, error) {
+	if right == 0 {
+		return 0, errors.New("division by zero")
+	}
+
+	return left / right, nil
 }
