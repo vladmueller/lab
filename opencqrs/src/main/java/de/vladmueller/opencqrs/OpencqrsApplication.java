37b7c618703eb9a5a3df058eb5fc67bfda512011
Vladimir Müller <code@vladmueller.de>
2025-10-26 12:34:25 +0100

    🎉 Bootstrap Spring Boot application with Maven Wrapper and initial configurations for OpenCQRS
    - Initialize a new Spring Boot application with Maven wrapper scripts (`mvnw` and `mvnw.cmd`).
    - Add basic project structure including main and test classes.
    - Configure `pom.xml` with required dependencies.
    - Add `.gitignore` and `.gitattributes` for common IDEs and build files.
    - Include `application.properties` with basic application and ESDB settings.

---
+++ opencqrs/src/main/java/de/vladmueller/opencqrs/OpencqrsApplication.java
@@ -0,0 +1,13 @@
+package de.vladmueller.opencqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpencqrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpencqrsApplication.class, args);
+    }
+
+}
